abdfedcce454577dddb88e7477af3beb12d0b7e3
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-08-01 22:24:57 +0100

    Make a Custom User Authentication Model

--- full_stack_blog/settings.py
+++ full_stack_blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@48za1sr0dj6q+mrz56!8z(&p+8&g_$&#@=2&$8w80g&q-o%b%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'users.apps.UsersConfig'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# User Authentication Model
+
+AUTH_USER_MODEL = 'users.User'
--- users/models.py
+++ users/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+import random, string
+
+def getUserId(): 
+    allowed = list(string.ascii_letters + string.digits)
+    return ''.join([random.choice(allowed) for _ in range(11)])
+
+# class StaffTitle(models.Model) :
+#     title = models.CharField(200)
+
+#     class Meta :
+#         constraints = [
+#             models.UniqueConstraint(fields=['title'], name='stafftitle_title_unique')
+#         ]
+
+class User(AbstractUser) :
+    id = models.CharField(primary_key=True, default=getUserId, max_length=11)
+    # staff_title = models.ForeignKey(StaffTitle, on_delete=models.SET_NULL, null=True, blank=True)
+    staff_title = models.CharField(max_length=200, null=True, blank=True)
+    about = models.CharField(max_length=500, null=True, blank=True)
+
+    # User profil image is missing
+
+    def save(self, *args, **kwargs) :
+        if self.is_staff :
+            if self.staff_title is None or self.staff_title == '':
+                raise Exception('Staff members should have staff title registered')
+
+            if self.first_name is None or self.first_name == '' :
+                raise Exception('First name is required for staff members')
+
+            if self.last_name is None or self.last_name == '' :
+                raise Exception('Last name is required for staff members')
+
+            if self.about is None or self.about == '' :
+                raise Exception('About text is required for staff members')
+        
+        if not self.is_staff or not self.is_superuser :
+            self.staff_title = None 
+
+        super().save(*args, **kwargs)
+
