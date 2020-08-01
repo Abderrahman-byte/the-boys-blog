54c31a091eba5f26fe631471ec88ef2198701349
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-08-01 22:51:56 +0100

    Create Blog models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+from uuid import uuid4
+
+class Category(models.Model) :
+    id = models.UUIDField(primary_key=True, default=uuid4)
+    title = models.CharField(max_length=200)
+    short_title = models.CharField(max_length=200)
+
+    class Meta :
+        constraints = [
+            models.UniqueConstraint(fields=['title'], name='category_unique_title'),
+            models.UniqueConstraint(fields=['short_title'], name='category_unique_short_title'),
+        ]
+
+class Article(models.Model) :
+    id = models.UUIDField(primary_key=True, default=uuid4)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category)
+    title = models.CharField(max_length=500)
+    content = models.TextField()
+
+    class Meta :
+        constraints = [
+            models.UniqueConstraint(fields=['title'], name='article_unique_title')
+        ]
+
+    def save(self, *args, **kwargs) :
+        if not self.author.is_staff :
+            raise Exception('Only staff members can publish articles')
+        
+        super().save(*args, **kwargs)
+
+class Comment(models.Model) :
+    id = models.UUIDField(primary_key=True, default=uuid4)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    User = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
