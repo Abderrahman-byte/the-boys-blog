a5c572def42182db2587576bf01c2db1c4a3f614
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-08-09 11:37:51 +0100

    serve media files

--- full_stack_blog/settings.py
+++ full_stack_blog/settings.py
@@ -137,7 +137,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'react-ui/build/static')
 ]
 
-MEDIA_URL = '/url/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # User Authentication Model
--- full_stack_blog/urls.py
+++ full_stack_blog/urls.py
@@ -16,13 +16,19 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
+from django.conf.urls.static import static
+from django.conf import settings
 
 from blog.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+]
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [
     # Render index.html in all cases
     path('', TemplateView.as_view(template_name='index.html')),
     ### re_path(r'^.*/', TemplateView.as_view(template_name='index.html')),
