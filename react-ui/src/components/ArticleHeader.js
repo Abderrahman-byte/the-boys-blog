edbfc881d576befe8a4d23dbfdc1c81dea3e293f
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-09-04 08:39:20 +0100

    Add tooltips to article buttons control

--- react-ui/src/components/ArticleHeader.js
+++ react-ui/src/components/ArticleHeader.js
@@ -7,8 +7,8 @@ export const ArticleHeader = ({author, pubdate}) => {
     return (
         <div className='ArticleHeader'>
             <div className='control'>
-                <Link className='edit' data-title='Edit Article'><i className='fas fa-cog'></i></Link>
-                <Link className='delete' data-title='Delete Article'><i className='fas fa-trash'></i></Link>
+                <Link className='edit tooltip tooltip-right'  data-title='Edit Article'><i className='fas fa-cog'></i></Link>
+                <Link className='delete tooltip tooltip-top' data-title='Delete Article'><i className='fas fa-trash'></i></Link>
             </div>
             <p>Published at {(new Date(pubdate)).toLocaleString()} <span>|</span> by <Link className='author'>{author.first_name} {author.last_name}</Link></p>
         </div>
