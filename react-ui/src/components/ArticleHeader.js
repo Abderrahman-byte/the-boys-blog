d41e79320a4cf33dcc083fab767f31e370a22aa9
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-09-04 08:46:24 +0100

    Render article control buttons if the user is the author

--- react-ui/src/components/ArticleHeader.js
+++ react-ui/src/components/ArticleHeader.js
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { AuthContext } from '../context/AuthContext'
 
 import '../styles/ArticleHeader.scss'
 
 export const ArticleHeader = ({author, pubdate}) => {
+    const { user } = useContext(AuthContext)
+    
     return (
         <div className='ArticleHeader'>
             <div className='control'>
-                <Link className='edit tooltip tooltip-right'  data-title='Edit Article'><i className='fas fa-cog'></i></Link>
-                <Link className='delete tooltip tooltip-top' data-title='Delete Article'><i className='fas fa-trash'></i></Link>
+                {user.id === author.id ? (
+                    <>
+                        <Link className='edit tooltip tooltip-right'  data-title='Edit Article'><i className='fas fa-cog'></i></Link>
+                        <Link className='delete tooltip tooltip-top' data-title='Delete Article'><i className='fas fa-trash'></i></Link>
+                    </>
+                ) : (null)}    
             </div>
             <p>Published at {(new Date(pubdate)).toLocaleString()} <span>|</span> by <Link className='author'>{author.first_name} {author.last_name}</Link></p>
         </div>
