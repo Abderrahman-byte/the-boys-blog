4e7bb697c16a630dcfd688b1fb4031eb4a502b6a
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-09-09 09:52:12 +0100

    Create Skeleton Component

---
+++ react-ui/src/components/CommentSkelt.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import '../styles/CommentSkelton.scss'
+
+export const CommentSkelton = () => {
+    return (
+        <div className='CommentItem CommentSkelton'>
+            <div className='profil'>
+                <img className='avatar' src={`http://localhost:8000/media/users/skeleton.png`} />
+                <span></span>
+            </div>
+
+            <div className='info'>
+                <p className='name'></p>
+                <p className='posted_date'></p>
+                <div className='content'></div>
+            </div>
+        </div>
+    )
+}
--- react-ui/src/components/CommentsList.js
+++ react-ui/src/components/CommentsList.js
@@ -4,12 +4,14 @@ import '../styles/CommentsList.scss'
 
 import { CommentItem } from './CommentItem'
 import { CommentsContext } from '../context/CommentContext'
+import { CommentSkelton } from './CommentSkelt'
 
 export const CommentsList = () => {
-    const { data } = useContext(CommentsContext)
+    const { data, skelton } = useContext(CommentsContext)
 
     return (
         <div className='CommentsList'>
+            {skelton && (<CommentSkelton />)}
             {data.map(item => <CommentItem key={item.id} data={item} />)}
         </div>
     )
