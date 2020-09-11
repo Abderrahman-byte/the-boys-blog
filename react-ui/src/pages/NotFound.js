f4ea9bc83587dd2635714319bc13d3a381c1a697
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-09-11 14:41:37 +0100

    add style to 404 component

--- react-ui/src/pages/NotFound.js
+++ react-ui/src/pages/NotFound.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import '../styles/NotFound.scss'
 
 export const NotFound = () => {
     return (
         <div className='NotFound'>
-            <p>This page does not exists you f*****g moran</p>
+            <h1>Error</h1>
+            <span className='status'>404</span>
+            <p className='msg'>The page requested was not found</p>
+            <Link className='btn btn-elt btn-primary' to='/'>Return Home</Link>
         </div>
     )
 }
