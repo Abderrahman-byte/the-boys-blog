f889053bafeb31f34ff56362335228808d07c17c
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-09-12 10:37:56 +0100

    fetch categories data

--- react-ui/src/context/CategoriesContext.js
+++ react-ui/src/context/CategoriesContext.js
@@ -1,8 +1,29 @@
-import React, { createContext } from 'react'
+import React, { createContext, useState, useEffect, useContext } from 'react'
+
+import { ModelsContext } from './ModelsContext'
+import { LoadingModel} from '../components/LoadingModel'
 
 export const CategoriesContext = createContext({})
 
 export const CategoriesProvider = ({children}) => {
+    const { openModel, closeModel } = useContext(ModelsContext)
+    const [categoriesList, setCategoriesData] = useState([])
+    
+    const getCategories = async () => {
+        openModel(<LoadingModel />, false)
+        const req = await fetch('http://localhost:8000/api/categories/')
+        if(req.status >= 200 && req.status < 300) {
+            const data = await req.json()
+            console.log(data)
+            setCategoriesData(data)
+        }
+        setTimeout(closeModel, 1000)
+    }
+
+    useEffect(() => {
+        getCategories()    
+    }, [])
+
     return (
         <CategoriesContext.Provider values={{}}>
             {children}
