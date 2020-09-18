77f37bd4b184f06b86d7399b74e66079b85fa1ba
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-09-18 14:41:24 +0100

    set up more staff btn

--- react-ui/src/pages/Team.page.js
+++ react-ui/src/pages/Team.page.js
@@ -6,12 +6,14 @@ export const TeamPage = ({setDefault, setWallpaperImg, setHeaderText}) => {
     const { openModel, closeModel } = useContext(ModelsContext)
     const [data, setData] = useState([])
     const [currentPage, setPage] = useState(1)
-    const [itemsPerPage] = useState(10)
+    const [itemsPerPage] = useState(2)
 
     const [isMore, setMoreState] = useState(true)
     const [isLoading, setLoadingState] = useState(true)
 
     const getStaff = async () => {
+        if(!isMore) return
+
         setLoadingState(true)
         const offset = itemsPerPage * (currentPage - 1)
         const req = await fetch(`http://localhost:8000/api/staff?limit=${itemsPerPage}&offset=${offset}`)
@@ -27,6 +29,10 @@ export const TeamPage = ({setDefault, setWallpaperImg, setHeaderText}) => {
         }
         setLoadingState(false)
         closeModel()
+    }
+
+    const nextPage = () => {
+        setPage(currentPage + 1)
     }
 
     useEffect(() => {
@@ -46,7 +52,13 @@ export const TeamPage = ({setDefault, setWallpaperImg, setHeaderText}) => {
                     <TeamList
                         data={data}
                     />
-                </div>
+                    {isMore && !isLoading ? (
+                        <button 
+                        onClick={nextPage} 
+                        style={{ margin: '0 auto', padding: '.5em 1.75em', display: 'block'}} 
+                        className='btn btn-elt btn-primary'>More</button>
+                    ): null}
+                    </div>
                 <div className='aside'></div>
             </div>
         </div>
